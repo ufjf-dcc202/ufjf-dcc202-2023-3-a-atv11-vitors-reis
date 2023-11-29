abaaf7008482f83b555afcd192ae9981abaaf1a4
Vitor Reis <vitorsouza.reis@estudante.ufjf.br>
2023-11-29 20:06:23 -0300

    add listas e exportações

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
+import { getLista } from "./lista.js";
+
 const pEntrada = document.querySelector('#entrada');
 const olSaida = document.querySelector('#saida')
 const btnAdicionar = document.querySelector('#adicionar');
-const btn = document.querySelector('#limpar');
+const btnLimpar = document.querySelector('#limpar');
+
+olSaida.innerHTML = "";
+
+function atualizarItensDeLista() {
+    
+}
