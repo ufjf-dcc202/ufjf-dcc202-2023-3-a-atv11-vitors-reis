eb9337dcbecda136f9de6e55a62a3a02ffed9b49
Vitor Reis <vitorsouza.reis@estudante.ufjf.br>
2023-11-29 19:54:30 -0300

    adicionando main.js

---
+++ main.js
@@ -0,0 +1,4 @@
+const pEntrada = document.querySelector('#entrada');
+const olSaida = document.querySelector('#saida')
+const btnAdicionar = document.querySelector('#adicionar');
+const btn = document.querySelector('#limpar');
